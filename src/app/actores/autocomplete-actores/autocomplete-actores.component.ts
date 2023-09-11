d8b3bcaf82f7a86c434b09167970143016c6c42e
Giomar L <giomarlimocaldas1234@gmail.com>
2023-09-10 22:22:28 -0500

    Cambios listos sin back-end

--- src/app/actores/autocomplete-actores/autocomplete-actores.component.ts
+++ src/app/actores/autocomplete-actores/autocomplete-actores.component.ts
@@ -1,23 +1,57 @@
-import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatAutocompleteActivatedEvent, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-autocomplete-actores',
   templateUrl: './autocomplete-actores.component.html',
   styleUrls: ['./autocomplete-actores.component.css']
 })
-export class AutocompleteActoresComponent {
+export class AutocompleteActoresComponent implements OnInit{
 
   control: FormControl = new FormControl();
 
   actores = [
-            {nombre: 'Tom Holland 1', imagen: 'https://th.bing.com/th/id/OIP.wOYrylBWQUkmPkqnQZLbFQAAAA?pid=ImgDet&rs=1'},
-            {nombre: 'Tom Holland 2', imagen: 'https://th.bing.com/th/id/OIP.wOYrylBWQUkmPkqnQZLbFQAAAA?pid=ImgDet&rs=1'},
-            {nombre: 'Tom Holland 3', imagen: 'https://th.bing.com/th/id/OIP.wOYrylBWQUkmPkqnQZLbFQAAAA?pid=ImgDet&rs=1'},
+            {nombre: 'Tom Holland 1', personaje: '', imagen: 'https://th.bing.com/th/id/OIP.wOYrylBWQUkmPkqnQZLbFQAAAA?pid=ImgDet&rs=1'},
+            {nombre: 'Tom Holland 2', personaje: '', imagen: 'https://th.bing.com/th/id/OIP.wOYrylBWQUkmPkqnQZLbFQAAAA?pid=ImgDet&rs=1'},
+            {nombre: 'Tom Holland 3', personaje: '', imagen: 'https://th.bing.com/th/id/OIP.wOYrylBWQUkmPkqnQZLbFQAAAA?pid=ImgDet&rs=1'},
             ]
+
+  actoresOriginal = this.actores;
+
+  actoresSeleccionados = [];
+
+  columnasAMostrar = ['imagen', 'nombre', 'personaje', 'acciones'];
+
+  @ViewChild(MatTable) table: MatTable<any>;
+
+  ngOnInit(): void {
+    this.control.valueChanges.subscribe(valor => {
+      this.actores = this.actoresOriginal;
+      this.actores = this.actores.filter(actor => actor.nombre.indexOf(valor) != -1);
+    })
+  }
 
   optionSelected(event: MatAutocompleteSelectedEvent){
     console.log(event.option.value);
+    this.actoresSeleccionados.push(event.option.value);
+    this.control.patchValue('');
+    if(this.table !== undefined){
+      this.table.renderRows();
+    }
+  }
+
+  eliminar(actor){
+    const indice = this.actoresSeleccionados.findIndex(a => a.nombre == actor.nombre);
+    this.actoresSeleccionados.splice(indice, 1);
+    this.table.renderRows();
+  }
+
+  finalizaArrastre(event: CdkDragDrop<any[]>){
+    const indicePrevio = this.actoresSeleccionados.findIndex(actor => actor === event.item.data);
+    moveItemInArray(this.actoresSeleccionados, indicePrevio, event.currentIndex);
+    this.table.renderRows();
   }
 }
